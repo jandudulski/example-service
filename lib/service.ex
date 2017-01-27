227677c71e918d2cffd6edd05b6385457cf9cd2e
Jan Dudulski <jan@dudulski.pl>
2017-01-27 13:35:14 +0100

    Extact to_token

--- lib/service.ex
+++ lib/service.ex
@@ -7,8 +7,10 @@ defmodule Service do
     import Supervisor.Spec, warn: false
 
     port = to_port(Application.get_env(:service, :port))
+    token = to_token(Application.get_env(:service, :token))
+
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, Service.Web, [], [port: port])
+      Plug.Adapters.Cowboy.child_spec(:http, Service.Web, [], [port: port, token: token])
     ]
     opts = [strategy: :one_for_one, name: Service.Supervisor]
 
@@ -22,4 +24,11 @@ defmodule Service do
   defp to_port(value) when is_integer(value), do: value
   defp to_port(value) when is_binary(value), do: String.to_integer(value)
   defp to_port({:system, env_var}), do: to_port(System.get_env(env_var))
+
+  defp to_token(nil) do
+    Logger.warn("Using default token")
+    "secret"
+  end
+  defp to_token(value) when is_binary(value), do: value
+  defp to_token({:system, env_var}), do: to_token(System.get_env(env_var))
 end
